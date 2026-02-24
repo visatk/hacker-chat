168a56f5f41d085cda3a364c9bd857dec1dd74bc
visatk <visatk29@gmail.com>
2026-02-24 15:30:07 +0600

    Create vite.config.ts

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
+import { cloudflare } from "@cloudflare/vite-plugin";
+
+export default defineConfig({
+  plugins: [
+    tailwindcss(),
+    react(),
+    cloudflare()
+  ]
+});
